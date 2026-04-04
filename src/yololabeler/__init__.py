746b5a83b4a817164a11125bd0836a28918012c5
ZackLoken <zack@savannainstitute.org>
2026-04-04 18:34:10 -0500

    Update __init__.py

--- src/yololabeler/__init__.py
+++ src/yololabeler/__init__.py
@@ -1,5 +1,5 @@
 """YoloLabeler - lightweight YOLO annotation tool (detection + segmentation)."""
 
-from .annotator import YoloLabeler, main  # noqa: F401
+from .gui import YoloLabeler, main  # noqa: F401
 
-__version__ = "4.0.0"
+__version__ = "5.0.0"
